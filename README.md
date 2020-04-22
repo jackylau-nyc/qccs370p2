814fcaa34bf002356ddc17a07387dd9959416423
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-04-22 01:53:40 -0400

    Configured project directotry and added db connection files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# qccs370p2
-Mane's Software Engineering Group Project 
+# Queens College CSCI-370 Software Engineering  
+
+## Project 2: Hotel Reservation System
+
+## Team Members
+
+1. David Liu
+2. Jacky Lau
+3. Mei King Kwok
+4. Muneeb Khawaja
+5. Reveka Lee
+
+## Instructions
+
+### Configuring a Database
+
+1. Navigate to `/src/database`.
+2. Rename `db-credentials.php.example` to `db-credentials.php`.
+3. Assign appropriate values to variables in `db-credentials.php`.
+
+**Note**: `db-credentials.php` is already in .gitignore and will be ignored in subsequent commits.
+**Warning**: Never store credentials on GitHub.
---
+++ src/database/db-connection.php
@@ -0,0 +1,25 @@
+<?php
+
+    function openConnection() {
+        $connection = new mysqli($dbHost, $dbUser, $dbPass,$dbInstance, $port);
+        if ($connection->connect_error) {
+            echo "code red"; 
+            die('Connect Error (' . $connection->connect_errno . ') '. $connection->connect_error);
+        }
+        return $connection;
+    }
+    
+    function closeConnection($conn) {
+        $conn -> close();
+    }
+
+    function testConnection($conn){
+        // to-do
+    }
+    
+    function printConnectionProperties($conn){
+        echo "<pre>" . print_r( $conn, true ). "</pre>"; 
+    }
+
+?>
+   
