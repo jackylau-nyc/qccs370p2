f94c0bc941d30df27f6273c49acefc1e81853e84
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-04-25 21:58:52 -0400

    Switched from sqli to PDO

--- src/database/db-connection.php
+++ src/database/db-connection.php
@@ -1,47 +1,57 @@
 <?php
-    require 'db-credentials.php';
 
-    class DBLink {
+require_once 'db-credentials.php';
 
-        private function openConnection() {
-            $connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DATABASE, DB_PORT);
-             if ($connection->connect_error) { 
-                die('Connect Error (' . $connection->connect_errno . ') '. $connection->connect_error);
-            }
-            return $connection;            
+class DBLink {
+
+    private function openConnection() {
+        try{
+            $connection = new PDO(DSN, DB_USER, DB_PASS);
+        } catch(PDOException $e){
+            $connection = null; 
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
         }
-
-        private function query($sql){
-            $connection = $this->openConnection();
-            $result = mysqli_query($connection, $sql);
-           
-            if(!$this->validResult($result)){
-                echo "No record found!";
-                exit;    
-            }
-            $results = array();
-            while ($row = mysqli_fetch_assoc($result)){
-                $results[] = $row;
-            }
-            $connection -> close(); 
-            return $results;      
-        }
-
-        private function validResult($result){
-            return (mysqli_num_rows($result) == 0) ? false : true;  
-        }
-        
-        function printConnectionProperties(){
-            $connection = $this->openConnection();
-            echo "<pre>" . print_r( $connection, true ). "</pre>"; 
-            $connection -> close();
-        }
-
-
+        return $connection;            
     }
-    
-    
 
-?>
+    private function query($sql, $params){
 
- 
+        $connection = $this->openConnection();
+        $result     = $connection->prepare($sql);
+        $result->execute($params);
+        
+        if(!$this->validResult($result)){
+            echo "No record found!";
+            return null;    
+        }
+        return  $result->fetchALL(PDO::FETCH_ASSOC);  
+    }
+
+    private function validResult($result){
+        return ($result->rowCount() > 0);  
+    }
+
+    /****************************************************************************************************
+     ********************************************** Queries *********************************************
+     ****************************************************************************************************/
+
+
+    function getCustomerPassword($username){
+        $sql = "SELECT passwd 
+                FROM   customer 
+                WHERE  username=?";
+        $result = $this->query($sql, array($username));
+        return (is_null($result)) ? null : $result[0]["passwd"];
+    }
+
+    function getAdminPassword($username){
+        $sql = "SELECT passwd 
+                FROM   admin
+                WHERE  username=?";
+        $result = $this->query($sql, array($username));
+        return (is_null($result))? null : $result[0]["passwd"];
+    }
+        
+} // class
+    
