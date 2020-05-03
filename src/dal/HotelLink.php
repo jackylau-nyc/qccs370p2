13c67b4eeddd04c19662cc70c45be88c16509754
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-05-03 12:55:44 -0400

    Added queries to Hotel Link

--- src/dal/HotelLink.php
+++ src/dal/HotelLink.php
@@ -12,15 +12,32 @@ class HotelLink extends BaseLink {
                 WHERE   hotel.x_cord = ? AND hotel.y_cord = ?";
         $params = array ($hotelXCord, $hotelYCord);
         $result = $this->query($sql, $params);        
-        return (is_null($result))? false : true;
+        return (is_null($result))? false : $result;
     }
 
-    function findAllRooms(){
+    function getAllRooms($hotelXCord, $hotelYCord){
+        $sql = "SELECT* 
+                FROM    room
+                WHERE   room.x_cord = ? AND room.y_cord = ?";
+        $params = array ($hotelXCord, $hotelYCord);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
+    }
+
+    function getClassCount(){
+        $sql = "SELECT class, count(class)
+                FROM room 
+                WHERE  room.x_cord  = ? AND room.y_cord = ?
+                GROUP BY class";
+        $params = array ($hotelXCord, $hotelYCord);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
+    }
+
+    function getAvailClassCount(){
 
     }
-    function findRoomsLT(){
 
-    }
     function findRoomsGT(){
 
     }
@@ -29,3 +46,6 @@ class HotelLink extends BaseLink {
     }
     
 }
+
+$var = new HotelLink();
+echo print_r($var->getAllRooms(0,0));
