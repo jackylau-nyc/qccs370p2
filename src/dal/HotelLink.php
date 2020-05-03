1e67c1e36a8697641af66b0c8eff3171918a1915
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-05-03 16:02:00 -0400

    Completed a significant amount of the HotelLink class

--- src/dal/HotelLink.php
+++ src/dal/HotelLink.php
@@ -5,8 +5,11 @@ require_once __DIR__."/BaseLink.php";
 
 
 class HotelLink extends BaseLink {
-
-    function findCompany($hotelXCord, $hotelYCord){
+    /**
+     * @return array if parent company found, false otherwise.
+     * array( array("company"=> "Howard Resorts"));  
+     */
+    function getCompany($hotelXCord, $hotelYCord){
         $sql = "SELECT  hotel.company
                 FROM    hotel
                 WHERE   hotel.x_cord = ? AND hotel.y_cord = ?";
@@ -15,8 +18,12 @@ class HotelLink extends BaseLink {
         return (is_null($result))? false : $result;
     }
 
+    /**
+     * @return array  containing all rooms that belong to a hotel.
+     * array( array("room_num"=> "6", "class"=> "Cheap","price"=> "450"));  
+     */
     function getAllRooms($hotelXCord, $hotelYCord){
-        $sql = "SELECT* 
+        $sql = "SELECT  room_num, class, price
                 FROM    room
                 WHERE   room.x_cord = ? AND room.y_cord = ?";
         $params = array ($hotelXCord, $hotelYCord);
@@ -24,8 +31,48 @@ class HotelLink extends BaseLink {
         return (is_null($result))? false : $result;
     }
 
-    function getClassCount(){
-        $sql = "SELECT class, count(class)
+
+    function findRoomsLT($hotelXCord, $hotelYCord, $price){
+        $sql = "SELECT  room_num, class, price
+                FROM    room
+                WHERE   room.x_cord = ? 
+                        AND room.y_cord = ? 
+                        AND room.price < ?";
+        $params = array ($hotelXCord, $hotelYCord, $price);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
+
+    }
+
+    function findRoomsGT($hotelXCord, $hotelYCord, $price){
+        $sql = "SELECT  room_num, class, price
+                FROM    room
+                WHERE   room.x_cord = ? 
+                        AND room.y_cord = ? 
+                        AND room.price > ?";
+        $params = array ($hotelXCord, $hotelYCord);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
+
+    }
+    function findRoomsEQ($hotelXCord, $hotelYCord, $price){
+        $sql = "SELECT  room_num, class, price
+                FROM    room
+                WHERE   room.x_cord = ? 
+                        AND room.y_cord = ? 
+                        AND room.price = ?";
+        $params = array ($hotelXCord, $hotelYCord);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
+        
+    }
+
+    /*
+     * Returns an arra containing arrays of room classifications and classification counts.  
+     * array( array("class"=> "Cheap","size"=> "3"), array("class"=> "Free,"size"=> "3") );  
+     */
+    function getClassCount($hotelXCord, $hotelYCord){
+        $sql = "SELECT class, count(class) as size
                 FROM   room 
                 WHERE  room.x_cord  = ? AND room.y_cord = ?
                 GROUP BY class";
@@ -34,15 +81,30 @@ class HotelLink extends BaseLink {
         return (is_null($result))? false : $result;
     }
 
-    function getAvailClassCount(){
+    function getAvailClassCount($hotelXCord, $hotelYCord, $date){
+        $sql = "SELECT class, count(class) as availableRooms
+                FROM  room 
+                WHERE room.room_num NOT IN (           
+                     SELECT room.room_num 
+                     FROM room 
+                     INNER JOIN room_has_reservation
+                        ON  room.x_cord = 0 
+                        AND room.y_cord = 0 
+                        AND room.x_cord = room_has_reservation.x_cord  
+                        AND room.y_cord = room_has_reservation.y_cord
+                        AND room_has_reservation.room_num =  room.room_num 
+                     INNER JOIN reservation
+                        ON  reservation.res_id = room_has_reservation.res_id
+                        AND  '2019-09-30' BETWEEN  
+                        reservation.res_start AND reservation.res_end)
+                GROUP BY room.class";
 
+        $params = array ($hotelXCord, $hotelYCord, $date);
+        $result = $this->query($sql, $params);        
+        return (is_null($result))? false : $result;
     }
 
-    function findRoomsGT(){
-
-    }
-    function findRoomsEQ(){
-        
-    }
     
 }
+$var = new HotelLink();
+echo var_dump( $var->getAvailClassCount(0,0, '2019-04-07')  );
