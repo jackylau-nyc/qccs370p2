9fdb0e5880834251472df8877368b88a3648b290
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-05-03 01:13:55 -0400

    Finished basic Map Controller and added a GET route

--- src/map/MapController.php
+++ src/map/MapController.php
@@ -2,6 +2,13 @@
 
 namespace map; 
 
-/** 
- *  
-*/
+require_once __DIR__."/MapAccessService.php";
+
+class MapController{
+    private static $mapAccSvc; 
+
+    static function getHotels(){
+        self::$mapAccSvc  = new MapAccessService(); 
+        echo  self::$mapAccSvc->getHotels();
+    } 
+}
