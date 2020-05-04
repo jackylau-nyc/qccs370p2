135323e04fcbc6848b04ac411b35db56eefe39c1
heik922 <heik922@yahoo.com.hk>
2020-05-04 11:25:04 -0400

    update login form

--- public/routes.php
+++ public/routes.php
@@ -46,7 +46,7 @@ $router->get('/admin', function() {
     require __DIR__.'./index.php;';
 });
 
-$router->post('/', function($req){
+$router->post('/signin', function($req){
     AccountController::customerSignIn($req->getBody());
 });
 
