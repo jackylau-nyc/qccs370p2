9fdec5851f8ea73ad906f0176594e1a1eab7a3da
Muneeb Khawaja <muneebtahirkhawaja@gmail.com>
2020-05-21 11:01:06 -0400

    Minor bug fixes

--- src/hotel/HotelAccessService.php
+++ src/hotel/HotelAccessService.php
@@ -15,6 +15,7 @@ require_once __DIR__."/../dal/HotelLink.php";
 class HotelAccessService{
     private $conn;
     private CONST ERR_MSG = "Error: Unable to process hotel GET request";
+    
     function __construct(){
         $this->conn = new  HotelLink(); 
     }
@@ -25,28 +26,28 @@ class HotelAccessService{
 
     function getAllRooms($hotelXCord, $hotelYCord){
         $res = $this->conn->getAllRooms($hotelXCord, $hotelYCord);
-        return (!$res)? $this->ERR_MSG : json_encode($res); 
+        return (!$res)? self::ERR_MSG : json_encode($res); 
     }
 
     function getAvailableRooms($hotelXCord, $hotelYCord, $date){
         $res = $this->conn->getAvailClassCount($hotelXCord, $hotelYCord, $date);
-        return (!$res)? $this->ERR_MSG : json_encode($res); 
+        return (!$res)? self::ERR_MSG : json_encode($res); 
     }
 
     function getRoomClasses($hotelXCord,$hotelYCord){
         $res = $this->conn->getRoomClasses($hotelXCord, $hotelYCord);
-        return (!$res)? $this->ERR_MSG : json_encode($res); 
+        return (!$res)? self::ERR_MSG : json_encode($res); 
     }
 
     function getAvailableRoomRecords($hotelXCord, $hotelYCord, $date){
        $res = $this->conn->getRoomRecords($hotelXCord, $hotelYCord, $date);
-       return (!$res)? $this->ERR_MSG : json_encode($res); 
+       return (!$res)? self::ERR_MSG : json_encode($res); 
     }
 
 
     function getRoomClassCounts($hotelXCord,$hotelYCord){
         $res = $this->conn->getClassCounts($hotelXCord, $hotelYCord);
-        return (!$res)? $this->ERR_MSG : json_encode($res); 
+        return (!$res)? self::ERR_MSG : json_encode($res); 
     }
     
 
