c4e9d2ec930d6baece4164cb053098a4c5ce433a
Jacky Lau <jackylau.email@gmail.com>
2020-05-21 18:26:06 -0400

    redirect to reservation

--- public/scripts/js/hotel-room.js
+++ public/scripts/js/hotel-room.js
@@ -45,7 +45,7 @@ function createRes(x, y, username, start, end, roomID) {
 
       form.method = "POST";
       form.action = "/reservation";
-      form.target = "_blank";   
+      form.target = "/reservation.php";
 
       element1.value=username;
       element1.name="username";
